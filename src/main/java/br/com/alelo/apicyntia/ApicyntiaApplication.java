ee3bf32c0159fd73315c9e4b0630c4467299e80a
cyntiagoulart <cyntiartg@gmail.com>
2020-11-10 17:53:25 -0300

    Primeiro commit referente a criação do projeto no STS.

---
+++ src/main/java/br/com/alelo/apicyntia/ApicyntiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.alelo.apicyntia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicyntiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicyntiaApplication.class, args);
+	}
+
+}
