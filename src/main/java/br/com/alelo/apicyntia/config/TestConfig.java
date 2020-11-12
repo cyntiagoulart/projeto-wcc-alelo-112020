ba478110b8ee26d66c5e151492801fc936cd2737
cyntiagoulart <cyntiartg@gmail.com>
2020-11-12 19:15:43 -0300

    Correção de bug de relacionamento entre tabelas e categorias do banco de dados.

--- src/main/java/br/com/alelo/apicyntia/config/TestConfig.java
+++ src/main/java/br/com/alelo/apicyntia/config/TestConfig.java
@@ -39,18 +39,20 @@ public class TestConfig implements CommandLineRunner {
 
 		Category cat1 = new Category(null, "Livros");
 		Category cat2 = new Category(null, "Ebooks");
-		Category cat3 = new Category(null, "Jornais");
-		Category cat4 = new Category(null, "Revistas");
+		
 		
 		Product p1 = new Product(null, "Saga Otori - A rede do céu é vasta - prelúdio by Lian Hearn", "Lorem ipsum dolor sit amet, consectetur.", 64.8, "");
 		Product p2 = new Product(null, "Saga Otori - A relva por travesseiro: 2 by Lean Hearn", "Nulla eu imperdiet purus. Maecenas ante.", 36.0, "");
 		Product p3 = new Product(null, "Turma da Mônica Geração 12 - Número Zero by Mauricio de Souza", "Nam eleifend maximus tortor, at mollis.", 6.0, "");
-		Product p4 = new Product(null, "Mon Tricot - Edição de Inverno 2019", "Donec aliquet odio ac rhoncus cursus.", 14.0, "");
-		Product p5 = new Product(null, "Diário de São Paulo - Edição Comemorativa", "Cras fringilla convallis sem vel faucibus.", 100.00, ""); 
 
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3, cat4));
-		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));
 		
+		p1.getCategories().add(cat1);
+		p2.getCategories().add(cat2);
+		p3.getCategories().add(cat1);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3));		
 		
 		User u1 = new User(null, "Sirigueijo", "sirigueijo@siricascudo.com", "977777777", "123456");
 		User u2 = new User(null, "Patrick Estrela", "patrick@estrela.com", "988888888", "123456");
