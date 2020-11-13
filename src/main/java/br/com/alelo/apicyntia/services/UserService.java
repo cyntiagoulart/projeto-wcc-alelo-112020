a288fbae54ce038501d6e2e635307cddf2b415b5
cyntiagoulart <cyntiartg@gmail.com>
2020-11-13 16:38:08 -0300

    Tratamento de exceções do findByID, delete e update.

--- src/main/java/br/com/alelo/apicyntia/services/UserService.java
+++ src/main/java/br/com/alelo/apicyntia/services/UserService.java
@@ -3,11 +3,17 @@ package br.com.alelo.apicyntia.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import br.com.alelo.apicyntia.entities.User;
 import br.com.alelo.apicyntia.repositories.UserRepository;
+import br.com.alelo.apicyntia.services.exceptions.DatabaseException;
+import br.com.alelo.apicyntia.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +27,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -29,13 +35,23 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
