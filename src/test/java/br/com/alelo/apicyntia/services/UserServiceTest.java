6f812e6a97d2c5a55f637c7e485a51e185c5b052
cyntiagoulart <cyntiartg@gmail.com>
2020-11-14 08:30:42 -0300

    Adição das dependências do JUnit e do Hamcrest no pom.xml e estrutura de testes no src/test.

---
+++ src/test/java/br/com/alelo/apicyntia/services/UserServiceTest.java
@@ -0,0 +1,58 @@
+package br.com.alelo.apicyntia.services;
+
+public class UserServiceTest {
+}
+
+/*
+ * @Test public void Test{ //Cenário
+ * 
+ * 
+ * //Ação
+ * 
+ * 
+ * //Teste
+ * 
+ * }
+ * 
+ * 
+ * import java.util.List; import java.util.Optional;
+ * 
+ * import javax.persistence.EntityNotFoundException;
+ * 
+ * import org.springframework.beans.factory.annotation.Autowired; import
+ * org.springframework.dao.DataIntegrityViolationException; import
+ * org.springframework.dao.EmptyResultDataAccessException; import
+ * org.springframework.stereotype.Service;
+ * 
+ * import br.com.alelo.apicyntia.entities.User; import
+ * br.com.alelo.apicyntia.repositories.UserRepository; import
+ * br.com.alelo.apicyntia.services.exceptions.DatabaseException; import
+ * br.com.alelo.apicyntia.services.exceptions.ResourceNotFoundException;
+ * 
+ * @Service public class UserService {
+ * 
+ * @Autowired private UserRepository repository;
+ * 
+ * public List<User> findAll() { return repository.findAll(); }
+ * 
+ * public User findById(Long id) { Optional<User> obj = repository.findById(id);
+ * return obj.orElseThrow(() -> new ResourceNotFoundException(id)); }
+ * 
+ * public User insert(User obj) { return repository.save(obj); }
+ * 
+ * public void delete(Long id) { try { repository.deleteById(id); } catch
+ * (EmptyResultDataAccessException e) { throw new ResourceNotFoundException(id);
+ * } catch (DataIntegrityViolationException e) { throw new
+ * DatabaseException(e.getMessage()); } }
+ * 
+ * public User update(Long id, User obj) { try { User entity =
+ * repository.getOne(id); updateData(entity, obj); return
+ * repository.save(entity); } catch (EntityNotFoundException e) { throw new
+ * ResourceNotFoundException(id); } }
+ * 
+ * private void updateData(User entity, User obj) {
+ * entity.setName(obj.getName()); entity.setEmail(obj.getEmail());
+ * entity.setPhone(obj.getPhone()); }
+ * 
+ * }
+ */
